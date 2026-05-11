471a7fbb45459a88bd9bec68095f713c1f4a020c
Gencom <75057223+cyrongau@users.noreply.github.com>
2026-05-11 10:33:00 +0300

    fix: import MessagesModule in SupportModule to resolve ChatGateway dependency

--- backend/src/support/support.module.ts
+++ backend/src/support/support.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SupportService } from './support.service';
 import { SupportController } from './support.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { MessagesModule } from '../messages/messages.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, MessagesModule],
   controllers: [SupportController],
   providers: [SupportService],
   exports: [SupportService],
