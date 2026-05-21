1d6203dc54a84dc05f75e6f1148c4a7c89488b8e
Gencom <75057223+cyrongau@users.noreply.github.com>
2026-05-21 21:07:35 +0300

    fix(android): create notification channel natively to prevent Bad notification crash

--- mobile/android/app/src/main/kotlin/com/kibabiinest/nest/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/com/kibabiinest/nest/MainActivity.kt
@@ -1,5 +1,23 @@
 package com.kibabiinest.nest
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterFragmentActivity()
+class MainActivity : FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "kibabii_navigation",
+                "Navigation Service",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager?.createNotificationChannel(channel)
+        }
+    }
+}
